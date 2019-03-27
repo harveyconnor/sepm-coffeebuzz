1dc00bc041e398e440cd451cbe642351e4053f4c
Harvey Connor <harveyconnor97@gmail.com>
2019-03-27 21:19:33 +1100

    Add rnn fixes & add root nav

--- index.js
+++ index.js
@@ -2,12 +2,44 @@ import { Navigation } from "react-native-navigation";
 
 import App from "./src/App";
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+Navigation.registerComponent('navigation.playground.WelcomeScreen', () => App);
 Navigation.events().registerAppLaunchedListener(() => {
+
   Navigation.setRoot({
     root: {
-      component: {
-        name: "navigation.playground.WelcomeScreen"
+      bottomTabs: {
+        children: [
+          {
+            component: {
+              name: 'navigation.playground.WelcomeScreenn',
+              passProps: {
+                text: 'This is tab 2'
+              },
+              options: {
+                bottomTab: {
+                  text: 'Tab 2',
+                  icon: require('./src/assets/home.png'),
+                  testID: 'SECOND_TAB_BAR_BUTTON'
+                }
+              }
+            }
+          },
+        {
+          component: {
+            name: 'navigation.playground.WelcomeScreenn',
+            passProps: {
+              text: 'This is tab 2'
+            },
+            options: {
+              bottomTab: {
+                text: 'Tab 2',
+                icon: require('./src/assets/home.png'),
+                testID: 'SECOND_TAB_BAR_BUTTON'
+              }
+            }
+          }
+        }
+      ]
       }
     }
   });
--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 16,
+  },
+  item: {
+    alignItems: 'center', 
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    margin: 8,
+    padding: 16,
+    backgroundColor: 'black'
+  }
+})
 
 export default class MyComponent extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Tesst</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.item}>
+          <Text style={{color: 'white'}}>Test</Text>
+        </View>
+        <View style={styles.item}>
+          <Text style={{color: 'white'}}>Test</Text>
+        </View>
+        <View style={styles.item}>
+          <Text style={{color: 'white'}}>Test</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
